690886264bc9c562b234ed75d21fa5c190d57c74
giorgi <zautashvili1994@gmail.com>
2025-01-19 22:47:30 +0400

    before footer style change

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,19 +4,22 @@ import { Link, useLocation } from "react-router-dom";
 import i18next from "i18next";
 import { Github, Linkedin, Twitter } from "lucide-react";
 import { AUTH_LAYOUT_PATHS } from "@/Routes/auth/index.enum";
+import { DEFAULT_LAYOUT_PATH } from "@/Routes/default/index.enum";
 
 const Footer: React.FC = () => {
   const { t } = useTranslation();
   const lang = i18next.language;
   const currentLang = lang ?? "en";
-  const  location = useLocation().pathname;
-  const homeLocation = location.slice(4,);
-  console.log(homeLocation)
+  const location = useLocation().pathname;
+  const isHome = location.slice(4)===DEFAULT_LAYOUT_PATH.HOME;//დინამიურად რომ შევცვალოთ bg-სტილი როცა home გვერძზე ვართ
+
 
   return (
-    <div className={` flex flex-col bg-[#fcfbf5] dark:bg-black  h-auto sm:h-[594px] dark:${homeLocation==="home"?"bg-custom-gradient":"bg-custom-gradient2"}`}>
-      <div className="sm:border-t   dark:border-white/10 sm:flex-row flex-col h-auto  sm:h-[494px] mx-auto flex w-full">
-        <div className=" w-full sm:w-[45%]  flex flex-col justify-center items-center   py-4 sm:py-0">
+    <div
+      className={` flex flex-col bg-[#fcfbf5]   h-auto sm:h-[594px] dark:${isHome? "bg-custom-gradient" : "bg-custom-gradient2"}`}
+    >
+      <div className=" sm:border-t  dark:border-white/10 sm:flex-row flex-col h-auto  sm:h-[494px] mx-auto flex w-full ">
+        <div className=" w-full sm:w-[45%]  flex flex-col justify-center items-center   py-4 sm:py-0 bg-blue-300">
           <div className=" h-full w-full sm:w-1/2 flex flex-col  pl-4 ">
             <div className=" h-1/2  flex justify-start items-end ">
               <img src="/images/footerLogo.png" alt="logo" />
@@ -53,30 +56,30 @@ const Footer: React.FC = () => {
             </div>
           </div>
         </div>
-        <div className="  sm:border-l py-4 sm:py-0  dark:border-white/10 w-full sm:w-[25%] flex  justify-start">
-          <div className=" flex flex-col pl-4  ">
-            <div className=" dark:text-[#F75A1D]  flex justify-center pt-0 sm:pt-[84px] items-end  font-bold text-3xl font-chakra-petch">
+        <div className=" border-t sm:border-t-0  sm:border-l py-4 sm:py-0  dark:border-white/10 w-full sm:w-[25%] flex  justify-start ">
+          <div className="border flex flex-col pl-4 w-full  ">
+            <div className="border dark:text-[#F75A1D]  flex justify-start pt-0 sm:pt-[84px] items-end  font-bold text-3xl font-chakra-petch">
               {t("footer.explore")}
             </div>
             <Link
               to={`/${currentLang}/${AUTH_LAYOUT_PATHS.PROFILE}`}
-              className="text-black  pt-10 dark:text-white dark:hover:text-blue2 hover:text-blue2 transition-all duration-300 ease-in"
+              className="text-black pt-2 border sm:pt-10 dark:text-white dark:hover:text-blue2 hover:text-blue2 transition-all duration-300 ease-in "
             >
               {t("footer.user-profile")}
             </Link>
           </div>
         </div>
-        <div className="border-l py-4 sm:py-0 dark:border-white/10 w-full sm:w-[30%] flex justify-evenly items-center">
-          <div className="pl-4 h-full  w-full sm:w-1/2 flex flex-col">
-            <div className=" dark:text-[#F75A1D]   pb-10 h-1/3 flex items-end text-3xl font-bold font-chakra-petch">
+        <div className="bg-blue-300 border-t sm:border-t-0  sm:border-l py-4 sm:py-0 dark:border-white/10 w-full sm:w-[40%] flex justify-evenly items-center ">
+          <div className="pl-4 h-full border w-full sm:w-1/2 flex flex-col">
+            <div className="border dark:text-[#F75A1D] pb-0  sm:pb-10 h-1/3 flex items-end text-3xl font-bold font-chakra-petch transition-all duration-300 ease-in  ">
               {t("footer.follow")}
             </div>
-            <div className=" h-1/2 flex flex-col text-black dark:text-white">
+            <div className="pt-2 border sm:pt-0 h-1/2 flex flex-col text-black dark:text-white">
               <a
                 href="https://github.com/giozauta"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="hover:text-blue2 transition-all duration-300 ease-in"
+                className="hover:text-blue2 transition-all duration-300 ease-in "
               >
                 <div className="pb-2 footerLinksHover">
                   {t("footer.github")}
@@ -106,16 +109,16 @@ const Footer: React.FC = () => {
               </div>
             </div>
           </div>
-          <div className=" h-full w-1/2 relative ">
+          <div className=" h-full w-1/2 relative hidden lg:flex ">
             <img
               src="/images/footer_image.webp"
-              className="hidden lg:flex  h-[650px]  absolute bottom-0 left-0"
+              className="  h-[650px]  absolute bottom-0 left-0"
               alt="footerImage"
             />
           </div>
         </div>
       </div>
-      <div className="border-t  dark:text-white dark:border-white/10 flex justify-center items-center h-[100px]">
+      <div className=" border-t  dark:text-white dark:border-white/10 flex justify-center items-center h-[100px]">
         {t("footer.copyright")}
       </div>
     </div>
