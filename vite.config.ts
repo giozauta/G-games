bd237370989e5d28d7faea71f3009c20a6734b89
giorgi <zautashvili1994@gmail.com>
2025-01-25 00:28:55 +0400

    versel vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,14 @@
-import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig(() => {
-  const rootPath = path.resolve(process.cwd()); // corrected 'rootPatch' to 'rootPath'
-  const srcPath = path.resolve(rootPath, "src"); // using rootPath here
-
-  return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        "~": rootPath,
-        "@": srcPath,
-      },
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./"),
+      "@": path.resolve(__dirname, "./src"),
     },
-  };
+  },
+  base: "/",
 });
