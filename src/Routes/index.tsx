ae5866c0cfd2317d3e10b6a1f68fe0478943d25f
giorgi <zautashvili1994@gmail.com>
2025-01-25 12:35:24 +0400

    test

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,9 +1,9 @@
 import LangGuard from "@/components/lang-guard";
 import IsAuthLayout from "@/layouts/auth";
 import DefaultLayout from "@/layouts/default";
-import NotFound from "@/pages/404";
+// import NotFound from "@/pages/404";
 
-import { Navigate, Route, Routes } from "react-router-dom";
+import {  Route, Routes } from "react-router-dom";
 import { AUTH_LAYOUT_ROUTES } from "./auth";
 import { DEFAULT_LAYOUT_ROUTES } from "./default";
 import { Suspense } from "react";
@@ -15,13 +15,11 @@ const AppRoutes = () => {
     <Suspense fallback={<Loading />}>
       <ScrollToTop />
       <Routes>
-        <Route path="/:lang/" element={<LangGuard />}>
+        <Route path="/:lang" element={<LangGuard />}>
           <Route element={<IsAuthLayout />}>{AUTH_LAYOUT_ROUTES}</Route>
           <Route element={<DefaultLayout />}>{DEFAULT_LAYOUT_ROUTES}</Route>
         </Route>
-        <Route path="/" element={<Navigate to="/en/home" />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/en/" element={<Navigate to="/en/home" />} />
+
       </Routes>
     </Suspense>
   );
